4fc5c0bc57d5536c228cba9444fe6e8677cd1466
Raul Dragoiu <dragoiuraul2009@yahoo.com>
2018-01-16 16:30:09 +0200

    user is done

--- frontend/src/app/components/pages/user/user.component.ts
+++ frontend/src/app/components/pages/user/user.component.ts
@@ -1,90 +1,87 @@
 import { Component, OnInit } from '@angular/core';
-import {User} from '../../../models/user';
+import { User } from '../../../models/user';
 import { MessageService } from 'primeng/components/common/messageservice';
-import {ApiService} from '../../../service/api.service'
+import { ApiService } from '../../../service/api.service'
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.less']
+    selector: 'app-user',
+    templateUrl: './user.component.html',
+    styleUrls: ['./user.component.less']
 })
 export class UserComponent implements OnInit {
 
-  displayDialog: boolean;
+    displayDialog: boolean;
 
-  user: User = new User();
-  
-  selectedUser: User;
-  
-  newUser: boolean;
+    user: User = new User();
 
-  users: User[];
+    selectedUser: User;
 
-  userDto: UserDto = new UserDto();
+    newUser: boolean;
 
-  constructor(private apiService: ApiService ) { 
-   
-  }
+    users: User[];
 
-  ngOnInit() {
-    this.refresh();
-  }
+    userDto: UserDto = new UserDto();
 
-  refresh(){
-    this.apiService.get('api/user').subscribe(res =>{    
-        this.users = res;
-        console.log("was called");
-       });
-  }
+    constructor(private apiService: ApiService) {
 
-  showDialogToAdd() {
-    this.newUser = true;
-    this.userDto = new UserDto();
-    this.displayDialog = true;
-}
-
-save() {
-    // this.user.updatedAt =  new Date().toLocaleDateString();
-    // this.user.createdAt =  new Date().toLocaleDateString();
-    this.apiService.post('api/user', this.userDto).subscribe(res =>{    
-        console.log(res);
-       });
-    this.displayDialog = false;
-    this.refresh();
-}
-
-edit(){
-    this.apiService.put('api/user/' + this.selectedUser.id, this.userDto).subscribe(res =>{    
-        console.log(res);
-    }); 
-    this.displayDialog = false;
-    this.refresh();
-}
-
-delete() {
-    this.apiService.delete('api/user/' + this.selectedUser.id).subscribe();
-    this.displayDialog = false;
-    this.refresh();
-}    
-
-onRowSelect(event) {
-    this.newUser = false;
-    this.userDto = this.cloneUserToUserDto(this.selectedUser);
-    console.log(this.userDto);
-    this.displayDialog = true;
-}
-
-cloneUserToUserDto(u: User): UserDto {
-    let userDto = new UserDto();
-    for(let prop in userDto) {
-      userDto[prop] = u[prop];
     }
-    return userDto;
-}
 
-findSelectedUserIndex(): number {
-    return this.users.indexOf(this.selectedUser);
-}
+    ngOnInit() {
+        this.refresh();
+    }
+
+    refresh() {
+        this.apiService.get('api/user').subscribe(res => {
+            this.users = res;
+            console.log("was called");
+        });
+    }
+
+    showDialogToAdd() {
+        this.newUser = true;
+        this.userDto = new UserDto();
+        this.displayDialog = true;
+    }
+
+    save() {
+        this.apiService.post('api/user', this.userDto).subscribe(res => {
+            this.refresh();
+        });
+        this.displayDialog = false;
+    }
+
+    edit() {
+        this.apiService.put('api/user/' + this.selectedUser.id, this.userDto).subscribe(res => {
+            this.refresh();
+        });
+        this.displayDialog = false;
+    }
+
+    delete() {
+        this.apiService.delete('api/user/' + this.selectedUser.id).subscribe(res => {
+            this.refresh();
+        });
+        this.displayDialog = false;
+    }
+
+    onRowSelect(event) {
+        this.newUser = false;
+        this.userDto = this.cloneUserToUserDto(this.selectedUser);
+        console.log(this.userDto);
+        this.displayDialog = true;
+    }
+
+    cloneUserToUserDto(u: User): UserDto {
+        let userDto = new UserDto();
+        for (let prop in userDto) {
+            userDto[prop] = u[prop];
+        }
+        return userDto;
+    }
+
+    findSelectedUserIndex(): number {
+        return this.users.indexOf(this.selectedUser);
+    }
 }
 
 export class UserDto {
